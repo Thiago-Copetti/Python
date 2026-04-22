f7e6682817c103ebcf30613b3983f3a54243e943
Thiago-Copetti <thiago.rc2009@hotmail.com>
2026-04-22 14:57:25 -0300

    5.Metros para centímetros

---
+++ Exercícios/5.metros_cm.py
@@ -0,0 +1,6 @@
+# Programa para transformar metros para centímetros
+
+metro = float(input('Informe a medida em metros : '))
+
+centimetro = metro * 100
+print(f'{metro} metros equivalem a {centimetro} centímetros')

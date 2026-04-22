b3fd63d1ffc3994a3137becc54c55710171344f5
Thiago-Copetti <thiago.rc2009@hotmail.com>
2026-04-22 14:54:56 -0300

    5-Tipo de variáveis

---
+++ Estudos Python/Aulas/5-tipos_de_variáveis.py
@@ -0,0 +1,27 @@
+# Os principais tipos de dados em Python são:
+
+# 1. int: para números inteiros, como 1, 2, 3, etc.
+# 2. float: para números decimais, como 3.14, 2.5, etc.
+# 3. str: para texto, como "Olá", "Python", etc.
+# 4. bool: para valores booleanos, que podem ser True ou False.
+
+# função type() para verificar o tipo de uma variável
+
+# Exemplo de uso:
+
+# Variável do tipo int
+idade = 25
+print(type(idade))  # Output: <class 'int'>
+
+# Variável do tipo float
+altura = 1.75
+print(type(altura))  # Output: <class 'float'>
+
+# Variável do tipo str
+nome = "Alice"
+print(type(nome))  # Output: <class 'str'>
+
+# Variável do tipo bool
+estudando = True
+print(type(estudando))  # Output: <class 'bool'>
+

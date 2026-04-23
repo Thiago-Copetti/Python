31e5afc441abad37efe16800e5d21964d3c4d1c8
Thiago-Copetti <thiago.rc2009@hotmail.com>
2026-04-23 19:35:49 -0300

    7-Boolen

---
+++ Estudos Python/Aulas/07-boolean.py
@@ -0,0 +1,13 @@
+# Tipo de dado bool (booleano) representa valores de verdade,
+#  ou seja, True (verdadeiro) e False (falso).
+
+# Exemplos de uso do tipo bool:
+# Variável do tipo bool
+estudando = True
+print(estudando)  # Output: True
+
+# Variável do tipo bool
+chovendo = False
+print(chovendo)  # Output: False
+
+# O tipo bool é frequentemente usado em estruturas de controle, como if, while, etc.

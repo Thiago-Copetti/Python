1f95d7e81d18173a574f1e20ccd33dc19ff18cd4
Thiago-Copetti <thiago.rc2009@hotmail.com>
2026-04-24 15:13:22 -0300

    09- inteiro e float

---
+++ Estudos Python/Aulas/09-int_float.py
@@ -0,0 +1,20 @@
+# Tipo int e float
+# O tipo int é usado para representar números inteiros, 
+# enquanto o tipo float é usado para representar números decimais.
+
+# Exemplo de uso:
+# Variável do tipo int
+numero_inteiro = 10
+
+# Variável do tipo float
+numero_decimal = 3.14
+
+print("Número inteiro:", numero_inteiro)  # Output: Número inteiro: 10
+
+print("Número decimal:", numero_decimal)  # Output: Número decimal: 3.14
+
+# a função type() mostra o tipo de dado da variável
+
+print("Tipo de numero_inteiro:", type(numero_inteiro))  # Output: Tipo de numero_inteiro: <class 'int'>
+print("Tipo de numero_decimal:", type(numero_decimal))  # Output: Tipo de numero_decimal: <class 'float'>
+

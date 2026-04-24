fd6d6640425e77c1dc40338b336d5069c2455419
Thiago-Copetti <thiago.rc2009@hotmail.com>
2026-04-24 15:14:56 -0300

    10- operadores aritméticos

---
+++ Estudos Python/Aulas/10-operadores_aritméticos.py
@@ -0,0 +1,44 @@
+# Operadores Aritméticos
+
+# + Adição
+# - Subtração
+# * Multiplicação
+# / Divisão
+
+# // Divisão inteira (retorna o quociente sem a parte decimal)
+# % Módulo (retorna o resto da divisão)
+# ** Exponenciação (eleva um número a uma potência)
+
+# Exemplo de uso dos operadores aritméticos:
+
+# soma +
+soma = 1 + 1
+print("1 + 1 = ", soma)
+
+# subtracao -
+subtracao = 1 - 1
+print("1 - 1 = ", subtracao)
+
+# multiplicacao *
+multiplicacao = 2 * 2
+print("2 x 2 = ", multiplicacao)
+
+# divisao /
+divisao = 5 / 2
+print("5 / 2 = ", divisao)
+print("Tipo da variavel do resultado da divisao = ", type(divisao))
+
+# int()
+print("Valor em inteiro = ", int(divisao))
+
+# float()
+print("Valor em float = ", float(divisao))
+
+# Modulo %
+modulo = 5 % 2
+print("Modulo = ", modulo)
+
+# divisao inteiro //
+divisao = 5 // 2
+print("5 // 2 = ", divisao)
+print("Tipo da variavel do resultado da divisao = ", type(divisao))

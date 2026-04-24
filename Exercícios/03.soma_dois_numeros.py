595ae57c69a1f2da5b4133aea2427c0da4f9f9a3
Thiago-Copetti <thiago.rc2009@hotmail.com>
2026-04-24 15:22:46 -0300

    03. soma entre dois números

---
+++ Exercícios/03.soma_dois_numeros.py
@@ -0,0 +1,8 @@
+#Faça um Programa que peça dois números e imprima a soma.
+
+numero_1 = float(input('Digite o primeiro número : '))
+numero_2 = float(input('Digite o segundo número : '))
+
+soma = numero_1 + numero_2
+
+print(f'A soma de {numero_1} e {numero_2} tem como resultado {soma}')

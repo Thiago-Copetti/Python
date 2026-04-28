3b21580e827d348d5920ba43a00ff5b1ee61721c
Thiago Copetti <thiago.rc2009@hotmail.com>
2026-04-28 18:07:19 -0300

    aula 18- Strings

---
+++ Estudos Python/Aulas/18-strings.py
@@ -0,0 +1,15 @@
+# Strings em Python
+
+# Strings são sequências de caracteres, utilizadas para armazenar e manipular texto.
+# As strings podem ser criadas usando aspas simples (' '), aspas duplas (" ")
+# ou aspas triplas (''' ''' ou """ """).
+# As strings são imutáveis, ou seja, uma vez criadas, não podem ser alteradas.
+# No entanto, podemos criar novas strings a partir de operações como concatenação 
+# e fatiamento. As string são iteráveis, ou seja, podemos percorrer cada caractere 
+# da string usando um loop.
+
+# Criando strings
+string1 = 'Olá, mundo!'  # Usando aspas simples
+string2 = "Olá, mundo!"  # Usando aspas duplas
+string3 = '''Olá, mundo!'''  # Usando aspas triplas
+

6f2105ab225b52a02e6a0619ce37cfb89044beb8
Thiago Copetti <thiago.rc2009@hotmail.com>
2026-04-29 14:19:06 -0300

    Aula 22- Concatenação

---
+++ Estudos Python/Aulas/22-concatenacao.py
@@ -0,0 +1,23 @@
+# Concatenação de strings
+
+# A concatenação de strings é o processo de unir duas 
+# ou mais strings para formar uma nova string. Em Python,
+#  isso pode ser feito de várias maneiras, incluindo o uso
+#  do operador de adição (+), o método join() e f-strings.
+
+# Usando o operador de adição (+)
+string1 = "Olá, "
+string2 = "mundo!"
+resultado = string1 + string2
+print(resultado)  # Saída: Olá, mundo!
+
+# Usando f-strings
+nome = "Alice"
+sobrenome = "Smith"
+resultado = f"{nome} {sobrenome}"
+print(resultado)  # Saída: Alice Smith
+
+# o operador de multiplicação (*) repete uma string
+repeticao = "Olá! " * 3
+print(repeticao)  # Saída: Olá! Olá! Olá!
+

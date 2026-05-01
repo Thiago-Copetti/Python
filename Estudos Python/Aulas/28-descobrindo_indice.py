8176cbe6c0e4b3b2f601f1d1059bb2fbd71bd781
Thiago Copetti <thiago.rc2009@hotmail.com>
2026-05-01 16:54:28 -0300

    Aula 28- Descobrindo índice

---
+++ Estudos Python/Aulas/28-descobrindo_indice.py
@@ -0,0 +1,11 @@
+# Descobrindo o Índice de um Elemento em uma Lista em Python
+
+# Criando uma lista de frutas
+frutas = ["maçã", "banana", "laranja", "uva"]
+
+# Descobrindo o índice de um elemento usando o método index()
+
+# indice = lista.index('item')
+
+indice_banana = frutas.index("banana")
+print(f"O índice de 'banana' é: {indice_banana}")  # Output: O índice de 'banana' é: 1

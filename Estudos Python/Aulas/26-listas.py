002c5153e7a01a1e6c0ab414faa7a6a8dc38e796
Thiago Copetti <thiago.rc2009@hotmail.com>
2026-05-01 16:53:08 -0300

    Aula 26- Listas

---
+++ Estudos Python/Aulas/26-listas.py
@@ -0,0 +1,25 @@
+# Listas em Python
+
+# As listas são estruturas de dados que podem armazenar uma coleção de itens. 
+# Elas são mutáveis, o que significa que você pode alterar seus elementos após a criação.
+#  As listas em Python são definidas usando colchetes [] e os elementos são separados 
+# por vírgulas.
+
+# Declaração de uma lista
+
+minha_lista = [1, 2, 3, 4, 5]
+
+# exibindo a lista
+
+print('Exibindo a lista : ', minha_lista)
+
+# acessando elementos da lista
+
+print('Acessando o primeiro elemento da lista : ', minha_lista[0])
+print('Acessando o terceiro elemento da lista : ', minha_lista[2])
+
+# listas em Python também suportam índices negativos, onde -1 é o último elemento
+
+print('Acessando o último elemento da lista : ', minha_lista[-1])
+print('Acessando o penúltimo elemento da lista : ', minha_lista[-2])
+

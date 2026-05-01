1d7fffd03e8ec4d5a8633a5f4ae3cbc64b052237
Thiago Copetti <thiago.rc2009@hotmail.com>
2026-05-01 16:53:51 -0300

    Aula 27- Modificando valor

---
+++ Estudos Python/Aulas/27-modificando_valor.py
@@ -0,0 +1,16 @@
+# Índices e Modificação de Listas em Python
+
+# Criando uma lista de frutas
+frutas = ["maçã", "banana", "laranja", "uva"]
+
+# Acessando elementos da lista usando índices
+
+print(frutas[0])  # Output: maçã
+print(frutas[1])  # Output: banana
+print(frutas[2])  # Output: laranja
+
+# Modificando elementos da lista usando índices
+
+frutas[1] = "abacaxi"  # Modificando "banana" para "abacaxi"
+print(frutas)  # Output: ['maçã', 'abacaxi', 'laranja', 'uva']
+
